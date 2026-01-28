599820abf3a35feb37fdd2819599a145d226eb49
Petrik Diak <diak@petrik.hu>
2026-01-28 12:21:39 +0100

    létrehoztam a tömböket

---
+++ general.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+
+const lista = [];
+
+const vezeteknevek = ["Kovács", "Szabó", "Tóth", "Nagy", "Varga",
+  "Kiss", "Molnár", "Balogh", "Farkas", "Papp"];
+
+const keresztnevek = ["János", "Péter", "László", "Gábor", "Zoltán",
+  "Anna", "Éva", "Katalin", "Mária", "Judit"]
+
+function randomElem(tomb) {
+  return tomb[Math.floor(Math.random() * tomb.length)];
+}
+
+
+function randomVerNyomas() {
+  const szisztoles = Math.floor(Math.random() * (160 - 90 + 1)) + 90;
+  const diasztoles = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
+  return `${szisztoles}/${diasztoles}`;
+}
+
+for (let i = 0; i < 20; i++) {
+  lista.push({
+    nev: `${randomElem(vezeteknevek)} ${randomElem(keresztnevek)}`,
+    vernyomas: randomVerNyomas()
+  });
+}
